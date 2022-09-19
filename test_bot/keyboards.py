459d34f330c8047db69bfd163391aea615c703c1
p0w3rh3ad <88826935+p0w3rh3ad@users.noreply.github.com>
2022-09-19 17:17:44 +0400

    Работаем над кнопками

---
+++ test_bot/keyboards.py
@@ -0,0 +1,17 @@
+from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove,\
+    InlineKeyboardMarkup, InlineKeyboardButton
+
+inline_kb = InlineKeyboardMarkup(row_width=2)
+reply_kb = ReplyKeyboardMarkup()
+
+# inline_btn_1 = InlineKeyboardButton('Button 1', callback_data='btn_1')
+# inline_btn_2 = InlineKeyboardButton('Button 2', callback_data='btn_2')
+# inline_btn_3 = InlineKeyboardButton('Button 3', callback_data='btn_3')
+
+# list_btn = [inline_btn_1, inline_btn_2, inline_btn_3]
+
+# for btn in list_btn:
+#     inline_kb.insert(btn)
+
+for btn in range(1,6):
+    inline_kb.insert(InlineKeyboardButton(f'Кнопка {btn}', callback_data=f'11{btn}'))
--- test_bot/my_bot.py
+++ test_bot/my_bot.py
@@ -2,6 +2,7 @@ from asyncore import dispatcher
 import logging
 from aiogram import Bot, Dispatcher, executor, types
 from config import TOKEN
+import keyboards as kb
 import random
 
 logging.basicConfig(level=logging.INFO)
@@ -31,12 +32,21 @@ def analize(msg):
         f'символов - {char_count}\n'\
         f'средняя длина слова - {word_len}'
 
+@dp.callback_query_handler()
+async def process_callback_kb(callback_query: types.CallbackQuery):
+    code = callback_query.data
+    await bot.answer_callback_query(callback_query.id,\
+        f'Нажата кнопка {code}')
+    # await bot.send_message(callback_query.from_user.id,\
+    #     f'Нажата кнопка {code}')
+
 @dp.message_handler(commands=['start'])
 async def welcome(message: types.Message):
     # await message.reply('Привет!')
     random.shuffle(preamble)
     random.shuffle(epilogue)
     await message.answer(f'{preamble[0]}\n{epilogue[0]}')
+    await message.answer('Тест кнопок', reply_markup=kb.inline_kb)
 
 @dp.message_handler()
 async def echo(message: types.Message):    
